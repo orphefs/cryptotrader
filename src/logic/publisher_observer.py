e43e0054fe58c6e25a2caec9ae60d8814ef58741
Orfeas Kypris <kypriso@gmail.com>
2018-03-10 19:32:45 +0000

    [autotrader-backtesting-dev] add temporary code

--- src/logic/publisher_observer.py
+++ src/logic/publisher_observer.py
@@ -1,3 +1,79 @@
+from typing import Callable
+
+
+class Event:
+    def __init__(self):
+        self.handlers = set()
+
+    def handle(self, handler):
+        self.handlers.add(handler)
+        return self
+
+    def unhandle(self, handler):
+        try:
+            self.handlers.remove(handler)
+        except:
+            raise ValueError("Handler is not handling this event, so cannot unhandle it.")
+        return self
+
+    def fire(self, *args, **kargs):
+        for handler in self.handlers:
+            handler(*args, **kargs)
+
+    def get_handler_count(self):
+        return len(self.handlers)
+
+    __iadd__ = handle
+    __isub__ = unhandle
+    __call__ = fire
+    __len__ = get_handler_count
+
+
+def download_data(client: BinanceClient):
+    pass
+
+
+def update_moving_average(new_data: Data, processed_stock_data: ProcessedStockData):
+    pass
+
+
+def buy_sell_x_amount(processed_stock_data: ProcessedStockData, strategy: Strategy):
+    pass
+
+
+def update_portfolio(portfolio: Portfolio):
+    pass
+
+
+def calculate_performance(portfolio: Portfolio)
+    pass
+
+
+class AutoTrader:
+    def __init__(self):
+        self.newDataAvailable = Event()
+        self.movingAverageUpdated = Event()
+        self.orderPlaced = Event()
+        self.portfolioUpdated = Event()
+        self.portfolio = Portfolio()
+        self.stock_data = StockData()
+        self.processed_stock_data = ProcessedStockData()
+
+    def add_handlers(self):
+        self.newDataAvailable += update_moving_average
+        self.movingAverageUpdated += buy_sell_x_amount
+        self.orderPlaced += update_portfolio
+        self.portfolioUpdated += calculate_performance
+
+    def trade(self):
+        data = download_data()
+        self.newDataAvailable(data)
+        self.movingAverageUpdated()
+        self.orderPlaced()
+        self.portfolioUpdated()
+
+
+
 class Subscriber:
     def __init__(self, name):
         self.name = name
