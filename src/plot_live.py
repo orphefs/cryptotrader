cd1de2b3f00ae059ffb9f3c6329a2926a89dc5e4
Orfeas Kypris <kypriso@gmail.com>
2018-05-19 13:27:10 +0100

    refactor plotting

---
+++ src/plot_live.py
@@ -0,0 +1,20 @@
+import os
+
+from matplotlib import pyplot as plt
+
+import definitions
+from live_logic.portfolio import Portfolio
+from plotting.plot_candles import custom_plot
+
+
+def postprocess():
+    portfolio = Portfolio.load_from_disk(os.path.join(definitions.DATA_DIR, "portfolio.dill"))
+    portfolio.compute_performance()
+    custom_plot(portfolio, None)
+    print(portfolio._point_stats['base_index_pct_change'])
+    print(portfolio._point_stats['total_pct_change'])
+
+    plt.show()
+
+if __name__ == '__main__':
+    postprocess()
