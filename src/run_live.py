414f9dcf4f1bdccd6daa70b686ea088969fbe79b
Orfeas Kypris <kypriso@gmail.com>
2018-05-21 15:14:40 +0100

    refactor live loop into class

--- src/run_live.py
+++ src/run_live.py
@@ -26,6 +26,74 @@ logger = logging.getLogger('cryptotrader_api')
 def get_capital_from_account(capital_security: str) -> float:
     return 5.0
 
+class LiveRunner:
+    def __init__(self, trading_pair, trade_amount):
+        self._trading_pair = trading_pair
+        self._trade_amount = trade_amount
+        self._start_time = start_time
+        self._stop_time = stop_time
+        self._current_candle = current_candle
+        self._current_prediction = current_prediction
+        self._current_signal = Hold(0, None)
+        self._previous_candle = previous_candle
+        self._previous_prediction = previous_prediction
+        self._previous_signal = Hold(0, None)
+        self._iteration_number = None
+        self._client = Client("", "")
+        self._kline_interval = Client.KLINE_INTERVAL_1MINUTE
+        self._parameters = LiveParameters(
+            update_period=timedelta(hours=1),
+            trade_amount=100,
+            sleep_time=1
+        )
+        self._portfolio =  Portfolio(initial_capital=get_capital_from_account(capital_security=None),
+                          trade_amount=self._parameters.trade_amount)
+        self._waiting_threshold = timedelta(seconds=45)
+
+        self._classifier = TradingClassifier.load_from_disk(os.path.join(definitions.DATA_DIR, "classifier.dill"))
+        self._market_maker = MarketMaker(self._client, self._trading_pair, self._trade_amount)
+
+    def initialize(self):
+        pass
+
+    def download_candle(self) -> Candle:
+        return download_live_data(self._client, self._trading_pair, self._kline_interval, 30)[-1]
+
+    def mock_download_candle(self) -> Candle:
+        return download_save_load(TimeWindow(start_time=datetime(2018, 5, 2),
+                                             end_time=datetime(2018, 5, 3)),
+                                  self._trading_pair, self._kline_interval).candles[self._iteration_number]
+
+    def run(self):
+        self._iteration_number = 1
+        while True:
+            # print("Loop iterating...")
+            # current_candle = download_live_data(client, trading_pair, Client.KLINE_INTERVAL_1MINUTE, 30)[-1]
+            self._current_candle = self.download_candle()
+            if is_time_difference_larger_than_threshold(self._current_candle, self._previous_candle, self._waiting_threshold,
+                                                        Candle.get_close_time_as_datetime):
+                logger.info("Registering candle: {}".format(self._current_candle))
+                self._classifier.append_new_candle(self._current_candle)
+                prediction = self._classifier.predict_one(self._current_candle)
+                print("Prediction is: {} on iteration {}".format(prediction, self._iteration_number))
+                if prediction is not None:
+                    self._current_signal = generate_trading_signal_from_prediction(prediction[0], self._current_candle)
+                    if self._current_signal.type == self._previous_signal.type:
+                        logger.info("Hodling...")
+                        pass  # HODL
+                    else:
+                        logger.info("Prediction for signal {}".format(self._current_signal))
+                        # order = market_maker.place_order(current_signal)
+                        self._portfolio.update(current_signal)
+                        self._portfolio.save_to_disk(os.path.join(definitions.DATA_DIR, "portfolio.dill"))
+                        self._previous_signal = self._current_signal
+                self._previous_candle = self._current_candle
+            time.sleep(self._parameters.sleep_time)
+            self._iteration_number += 1
+
+
+
+
 
 def run(trade_amount: float, capital_security: str, trading_pair: str):
     client = Client("", "")
@@ -48,6 +116,9 @@ def run(trade_amount: float, capital_security: str, trading_pair: str):
         Client.KLINE_INTERVAL_1MINUTE).candles
 
     # candles = download_live_data(client, trading_pair, Client.KLINE_INTERVAL_1MINUTE, 30)
+
+
+
     # for candle in candles:
     #     classifier.append_new_candle(candle)
     previous_candle = candles[0]
