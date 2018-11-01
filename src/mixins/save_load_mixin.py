9c8c970165db5fd7675c77b6073822b7a01369ce
Orfeas Kypris <kypriso@gmail.com>
2018-11-01 12:10:34 +0200

    handle absolute and relative paths

--- src/mixins/save_load_mixin.py
+++ src/mixins/save_load_mixin.py
@@ -5,11 +5,15 @@ from src import definitions
 
 class DillSaveLoadMixin:
     def save_to_disk(self, path_to_file: str):
+        if not os.path.isabs(path_to_file):
+            path_to_file = os.path.join(definitions.DATA_DIR, path_to_file)
         with open(os.path.join(definitions.DATA_DIR, path_to_file), 'wb') as outfile:
             dill.dump(self, outfile)
 
     @staticmethod
     def load_from_disk(path_to_file: str):
-        with open(os.path.join(definitions.DATA_DIR, path_to_file), 'rb') as outfile:
+        if not os.path.isabs(path_to_file):
+            path_to_file = os.path.join(definitions.DATA_DIR, path_to_file)
+        with open(path_to_file, 'rb') as outfile:
             obj = dill.load(outfile)
         return obj
