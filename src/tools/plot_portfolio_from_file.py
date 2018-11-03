4a028fe85d563a5840bf63928b2c87990e70addd
Orfeas Kypris <kypriso@gmail.com>
2018-11-03 15:44:30 +0200

    plot portfolio from file

---
+++ src/tools/plot_portfolio_from_file.py
@@ -0,0 +1,21 @@
+from argparse import ArgumentParser
+
+from src.live_logic.portfolio import Portfolio
+from src.plotting.plot_candles import custom_plot
+from src.tools.run_metadata import FullPaths
+import matplotlib.pyplot as plt
+
+def main(path_to_portfolio_dill: str):
+    predicted_portfolio = Portfolio.load_from_disk(path_to_portfolio_dill)
+    predicted_portfolio.compute_performance()
+    custom_plot(portfolio=predicted_portfolio, strategy=None, title='Backtesting run portfolio_df')
+    plt.show()
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description="Convert metadata dill into csv")
+    parser.add_argument("-i", dest="input_filename", required=False,
+                        help="input .dill", action=FullPaths)
+    args = parser.parse_args()
+    print("Input from {}".format(args.input_filename))
+    main(args.input_filename)
