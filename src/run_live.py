71c6f2ffbb55d2cd7bea7828c17862609a656930
Orfeas Kypris <kypriso@gmail.com>
2018-11-07 22:36:48 +0200

    refactor runners - live runner

--- src/run_live.py
+++ src/run_live.py
@@ -1,24 +1,39 @@
 import logging
 import os
+from typing import Tuple
 
 from src import definitions
+from src.definitions import DATA_DIR
 from src.resource_manager import runner
+from src.type_aliases import Path
 
 
-def run_live():
-    "Run the live algorithm with test orders (not placing real orders yet)."
+def run_live(trading_pair: str, trade_amount: float,
+                 path_to_log: Path = os.path.join(DATA_DIR,"live_run.log"),
+                 path_to_portfolio: Path = os.path.join(DATA_DIR,"live_portfolio.dill")) -> Tuple[Path, Path]:
+    "Run live, on real time exchange data."
+
 
     logging.basicConfig(
-        filename=os.path.join(definitions.DATA_DIR, 'live_run.log'), filemode='w',
+        filename=path_to_log, filemode='w',
         # stream=sys.stdout,
         level=logging.DEBUG,
     )
 
-    with runner(trading_pair="XRPBTC",
-                trade_amount=100,
-                run_type="live") as lr:
+    with runner(trading_pair=trading_pair,
+                trade_amount=trade_amount,
+                run_type="live",
+                path_to_portfolio=path_to_portfolio
+                ) as lr:
         lr.run()
+
+    return path_to_portfolio, path_to_log
 
 
 if __name__ == '__main__':
-    run_live()
+    path_to_portfolio, path_to_log = run_live("NEOBTC",
+                100,
+                os.path.join(DATA_DIR, "live_run.log"),
+                os.path.join(DATA_DIR, "live_portfolio.dill"))
+    print(path_to_portfolio, path_to_log)
+
