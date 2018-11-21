eb1dce5ba4f7aa11689fb381b72434f1ee2cf9e9
Orfeas Kypris <kypriso@gmail.com>
2018-11-21 20:47:45 +0200

    change timedelta.seconds to timedelta.total_seconds()

--- src/analysis_tools/analyze_batch_run.py
+++ src/analysis_tools/analyze_batch_run.py
@@ -15,25 +15,33 @@ def scan_folder(path_to_folder: Path) -> List[Path]:
         path_to_folder, "portfolio*.dill"))]
 
 
-def assemble_dataframe(paths_to_portfolios: List[Path]) ->  pd.DataFrame:
+def assemble_dataframe(paths_to_portfolios: List[Path]) -> pd.DataFrame:
     d = defaultdict(list)
     for path_to_portfolio in paths_to_portfolios:
         order_pairs, percentage_gains, index_performance, \
         classifier_time_window, testing_time_window = compute_all_statistics(path_to_portfolio)
-        d["training_time_window"].append(classifier_time_window)
-        d["training_duration"].append(classifier_time_window.duration)
-        d["testing_time_window"].append(testing_time_window)
-        d["testing_duration"].append(testing_time_window.duration)
+        d["training_time_start"].append(classifier_time_window.start_datetime)
+        d["training_time_end"].append(classifier_time_window.end_datetime)
+        d["training_duration_days"].append(classifier_time_window.duration.total_seconds()/(24* 3600))
+        d["testing_time_start"].append(testing_time_window.start_datetime)
+        d["testing_time_end"].append(testing_time_window.end_datetime)
+        d["testing_duration_days"].append(testing_time_window.duration.total_seconds()/(24*3600))
         d["index_performance"].append(index_performance.gains)
         d["percentage_gains"].append(percentage_gains.gains)
-        d["net_gains"].append(percentage_gains.gains - index_performance.gains)
+        net_gains = percentage_gains.gains - index_performance.gains
+        d["net_gains"].append(net_gains)
+        d["net_gains_per_day"].append(net_gains / (
+                testing_time_window.duration.total_seconds() / (24*3600)))
+
     return pd.DataFrame(d)
 
 
 def main():
     paths_to_portfolios = scan_folder(DATA_DIR)
     df = assemble_dataframe(paths_to_portfolios)
+    df.to_pickle(os.path.join(DATA_DIR, "batch_results.pkl"))
     print(df)
+
 
 if __name__ == '__main__':
     main()
