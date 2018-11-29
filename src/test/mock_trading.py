8bbf6421972d096c00418d154fb429b91b712519
Orfeas Kypris <kypriso@gmail.com>
2018-11-29 15:39:04 +0200

    implement decorator

--- src/test/mock_trading.py
+++ src/test/mock_trading.py
@@ -1,3 +1,4 @@
+import random
 from queue import Queue
 from typing import Optional, List, Any
 
@@ -5,6 +6,7 @@ from src.containers.order import Order, OrderID, Price, Size
 from src.containers.trading import Trading, Trade
 from src.containers.trading_pair import TradingPair
 from src.test.mock_client import MockClient
+from apscheduler.scheduler import Scheduler
 
 
 def _find_order_by_id(orders: List[Order], order_id: OrderID) -> Order:
@@ -15,10 +17,23 @@ def _find_latest_order(orders: List[Order]) -> Order:
     return max(orders, key=lambda x: x.timestamp)
 
 
+def _find_oldest_order(orders: List[Order]) -> Order:
+    return min(orders, key=lambda x: x.timestamp)
+
+
 def _print_list(lst: List[Any]):
     for element in lst:
         print(element)
     print("\n")
+
+
+def on_call(func):
+    def func_wrapper(self):
+        if random.choice([True, False]):
+            self._fill_orders()
+        func(self)
+        return func
+    return func_wrapper
 
 
 class MockTrading(Trading):
@@ -27,12 +42,20 @@ class MockTrading(Trading):
         self._filled_orders = []
         self._open_orders = []
         self._last_filled_order = None
+        self._scheduler = Scheduler()
 
+    def _fill_orders(self):
+        if len(self._open_orders) > 0:
+            self._filled_orders += self._open_orders.pop(self._open_orders.index(
+                _find_oldest_order(self._open_orders)))
+
+    @on_call
     def place_order(self, order: Order, ) -> Order:
         print("Place order {}".format(order))
         self._open_orders += order
         return order
 
+    @on_call
     def modify_order(self, order_id: OrderID, price: Price, size: Size) -> bool:
         found_order = _find_order_by_id(self._open_orders, order_id)
         order = _find_order_by_id(self._open_orders, order_id)
@@ -41,22 +64,26 @@ class MockTrading(Trading):
         print("Modify order {} \n to \n {}".format(order, found_order))
         return True
 
+    @on_call
     def get_open_orders(self, order_id: Optional[OrderID]) -> List[Optional[Order]]:
         print("Current open orders: \n")
         _print_list(self._open_orders)
         return self._open_orders
 
+    @on_call
     def cancel_order(self, order_id: OrderID) -> bool:
         order = _find_order_by_id(self._open_orders, order_id)
         self._open_orders.pop(self._open_orders.index(order))
         print("Cancel order {}".format(order))
         return True
 
+    @on_call
     def get_last_filled_order(self, trading_pair: TradingPair) -> Order:
         order = _find_latest_order(self._filled_orders)
         print("Last filled order: {}".format(order))
         return order
 
+    @on_call
     def get_order_history(self, trading_pair: TradingPair) -> List[Order]:
         print("Order history: \n")
         _print_list(self._filled_orders)
@@ -67,3 +94,4 @@ class MockTrading(Trading):
 
     def get_trades(self) -> List[Trade]:
         raise NotImplementedError
+
