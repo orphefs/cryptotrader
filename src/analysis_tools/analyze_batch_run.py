14204de0eac21da8d40ed5a84342ed101f57862d
Orfeas Kypris <kypriso@gmail.com>
2021-02-26 00:20:24 +0200

    refactor

--- src/analysis_tools/analyze_batch_run.py
+++ src/analysis_tools/analyze_batch_run.py
@@ -19,20 +19,24 @@ def assemble_dataframe(paths_to_portfolios: List[Path]) -> pd.DataFrame:
     d = defaultdict(list)
     for path_to_portfolio in paths_to_portfolios:
         print("Analyzing portfolio {}".format(path_to_portfolio))
-        order_pairs, percentage_gains, index_performance, \
-        classifier_time_window, testing_time_window = compute_all_statistics(path_to_portfolio)
-        d["training_time_start"].append(classifier_time_window.start_datetime)
-        d["training_time_end"].append(classifier_time_window.end_datetime)
-        d["training_duration_days"].append(classifier_time_window.duration.total_seconds()/(24* 3600))
-        d["testing_time_start"].append(testing_time_window.start_datetime)
-        d["testing_time_end"].append(testing_time_window.end_datetime)
-        d["testing_duration_days"].append(testing_time_window.duration.total_seconds()/(24*3600))
-        d["index_performance"].append(index_performance.gains)
-        d["percentage_gains"].append(percentage_gains.gains)
-        net_gains = percentage_gains.gains - index_performance.gains
+        run_statistics = compute_all_statistics(path_to_portfolio)
+
+        d["training_time_start"].append(run_statistics.classifier_time_window.start_datetime)
+        d["training_time_end"].append(run_statistics.classifier_time_window.end_datetime)
+        d["training_duration_days"].append(
+            run_statistics.classifier_time_window.duration.total_seconds() / (24 * 3600))
+        d["testing_time_start"].append(run_statistics.testing_time_window.start_datetime)
+
+        # TODO: investigate why .testing_time_window.end_datetime is not of type datetime
+        d["testing_time_end"].append(run_statistics.testing_time_window.end_datetime)
+        d["testing_duration_days"].append(
+            run_statistics.testing_time_window.duration.total_seconds() / (24 * 3600))
+        d["index_performance"].append(run_statistics.index_performance.gains)
+        d["percentage_gains"].append(run_statistics.percentage_gains.gains)
+        net_gains = run_statistics.percentage_gains.gains - run_statistics.index_performance.gains
         d["net_gains"].append(net_gains)
         d["net_gains_per_day"].append(net_gains / (
-                testing_time_window.duration.total_seconds() / (24*3600)))
+                run_statistics.testing_time_window.duration.total_seconds() / (24 * 3600)))
 
     return pd.DataFrame(d)
 
